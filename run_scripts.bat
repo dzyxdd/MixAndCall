10e6235c63c8be223da145225a0c7217454bd55c
DingZY <331646081@qq.com>
2024-02-22 02:40:26 +0800

    add album list and single list restructure the document

--- run_scripts.bat
+++ run_scripts.bat
@@ -7,5 +7,6 @@ python mix_generator.py
 python music_call_generator.py
 python stage_list_generator.py
 python EP_and_album_list_generator.py
+python single_list_generator.py
 
 cd ..
--- run_scripts.sh
+++ run_scripts.sh
@@ -6,5 +6,7 @@ cd python_generator
 python mix_generator.py
 python music_call_generator.py
 python stage_list_generator.py
+python EP_and_album_list_generator.py
+python single_list_generator.py
 
 cd ..
