6540bb99690f97379bfa839781d0e14eb26eb011
DingZY <331646081@qq.com>
2024-03-15 06:48:54 +0800

    add clean_cache.bat

---
+++ clean_cache.bat
@@ -0,0 +1,6 @@
+@echo off
+
+rd/s/q "docs/mix/auto-generated_html"
+rd/s/q "docs/music_lib/auto-generated_html"
+rd/s/q "docs/music_lib/music_call/auto-generated_html"
+rd/s/q "docs/music_lib/music_call/auto-generated_md"
