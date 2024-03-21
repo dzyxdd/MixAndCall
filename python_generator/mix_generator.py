6f7ef660d766d3dd0a8d8dfb83f2f0df63eb5c84
DingZY <331646081@qq.com>
2024-03-21 22:55:30 +0800

    修改mix通常长度标签接口为mix tag接口

--- python_generator/mix_generator.py
+++ python_generator/mix_generator.py
@@ -4,11 +4,23 @@ import os
 from jinja2 import Template
 
 
+def custom_sort(tag):
+    if tag.endswith('小節'):
+        return 0, int(tag[:-2])
+    else:
+        return 1, tag
+
+
 with open('../json_files/mix_list.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 with open('../templates/template_mix.html', 'r', encoding='utf-8') as f:
     template_string = f.read()
+
+# 对每个mix的mix_tag_list进行排序和去重
+for mix in data:
+    mix['mix_tag_list'] = list(set(mix['mix_tag_list']))
+    mix['mix_tag_list'].sort(key=custom_sort)
 
 template = Template(template_string)
 rendered_html = template.render(mix_list=data)
